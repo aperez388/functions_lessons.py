5bef1718fc26bc5afe39b5837897de31381b44b7
Perez <aperez388@cps.k12.il.us>
2025-01-07 15:05:59 -0600

    Los ojos chuecos

--- 2_functions.py
+++ 2_functions.py
@@ -1,6 +1,8 @@
+# RUBY SOLIS-CHAVEZ, ANABEL PEREZ
 # Create Functions Practice #1
 # Declare a function called greet, which every time it is called prints "Hello world!"
-
+def greet():
+    print("Hello, World!")
 # You should only define the function, you should not call it later.
 
 
@@ -11,9 +13,11 @@
 
 # Create Functions Practice #2
 # Declare a Function: Create a function called welcome that takes a person's name as an argument. This function should print "Welcome {name}!" whenever it is called.
+def welcome(name):
+    print(f"Welcome {name}!")
 
 # Create a Variable: Define a variable named name and store any name of your choice in it.
-
+name = "Jurgen"
 # Note: Only define the function and create the variable. Do not call the function afterwards.
 
 
@@ -23,7 +27,9 @@
 
 # Create Functions Practice #3
 # Declare a function called square, that takes any number as an argument, and each time it is called, it prints the square of that number on the screen (that is, the value to the second power).
-
+def square(num):
+    z = num * num
+    return z
 # The name of the argument that this function must take is number. Create this variable and assign it any number.
-
+num = 7
 # You should only define the function and create the variable, you should not call the function afterwards.
--- 3_return.py
+++ 3_return.py
@@ -1,6 +1,9 @@
+# RUBY SOLIS-CHAVEZ, ANABEL PEREZ
 # Return Practice #1
 # Create a function called power that takes two numeric values as arguments. It must return the number that results from solving a power, using the first number as the base, and the second as the exponent
-
+def power(x, y):
+    z = x**y
+    return z
 
 
 
@@ -9,19 +12,24 @@
 
 # Return Practice #2
 # Create a function called usd_to_eur that takes a numeric value (an amount in US dollars) as its only parameter, and returns the equivalent amount in euros as a result. For the purposes of this example, we will take the conversion 1 USD = 0.90 EUR.
-
+def usd_to_eur(us_dollars):
+    euro = us_dollars * 0.90
+    return euro
 # Create a variable called dollars and store any amount in it. Then, pass it to your function and evaluate its result.
-
+dollars = 66.00
 # Hint: to perform the conversion, the function internally must multiply this value in dollars by 0.90 to obtain the equivalent amount in euros.
-
+print(usd_to_eur(dollars))
 
 
 
 # Return Practice #3
 # Create a function called reverse_word that takes the characters of a given word as an argument, reverses the order of their characters, and returns them that way and in uppercase.
-
+def reverse_word(wordChar):
+    reversed = wordChar[::-1]
+    return reversed
 # For example, if we pass it the word "Python", it should return: "NOHTYP"
-
+print(reverse_word("PYTHON"))
 # Also, you must create a variable called word, which contains any string, to pass it as an argument to the created function.
-
+word = "JOEVER"
 # Hint: inside the created function, you should use string methods already seen.
+print(reverse_word(word))
